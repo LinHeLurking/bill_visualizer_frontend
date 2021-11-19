40de1e9c07da2a4c91d412855248caaaa9f718ae
LinHeLurking <LinHe.Lurking@gmail.com>
2021-11-19 10:15:19 +0800

    Simple table & install chart.js

--- src/components/AnalysisPage.vue
+++ src/components/AnalysisPage.vue
@@ -1,12 +1,67 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from "vue";
+import { useRoute } from "vue-router";
+import { NDataTable } from "naive-ui";
+
+interface MonthlyCost {
+    key: number,
+    month: number,
+    money: number,
+};
 
 export default defineComponent({
-    computed: {
-        queryId() {
-            return this.$route.params.queryId;
+    components: {
+        NDataTable,
+    },
+    setup() {
+        const route = useRoute()
+        var queryId = "";
+        var billTitle = "展示默认效果";
+        if (route.params.queryId) {
+            queryId = route.params.queryId as string;
         }
-    }
+        const getMonthlyCost = (): MonthlyCost[] => {
+            var cost: MonthlyCost[] = new Array();
+            if (queryId != "") {
+                // TODO: add API call here
+            } else {
+                var keyId = 0;
+                for (let mo = 1; mo <= 12; mo++) {
+                    cost.push({
+                        key: keyId++,
+                        month: mo,
+                        money: Math.round(Math.random() * 2000)
+                    });
+                }
+            }
+            return cost;
+        }
+        const dataTableInstRef = ref(null);
+        return {
+            queryId: queryId,
+            billTitle: billTitle,
+            dataTableInstRef: dataTableInstRef,
+            columns: [
+                {
+                    title: "Month",
+                    key: "month",
+                    sorter: {
+                        compare: (a: MonthlyCost, b: MonthlyCost) => a.month - b.month,
+                        multiple: 2,
+                    }
+                },
+                {
+                    title: "Money",
+                    key: "money",
+                    sorter: {
+                        compare: (a: MonthlyCost, b: MonthlyCost) => a.money - b.money,
+                        multiple: 1,
+                    }
+                },
+            ],
+            monthlyCost: getMonthlyCost(),
+        };
+    },
 })
 </script>
 
@@ -14,12 +69,32 @@ export default defineComponent({
 <template>
     <div class="analysis-page">
         <h2>账单分析页面内容</h2>
-        <h3>账单：{{ queryId }}</h3>
+        <h3>账单：{{ billTitle }}</h3>
+        <div class="monthly-cost-table-container">
+            <div class="monthly-cost-table-wrapper">
+                <n-data-table
+                    :columns="columns"
+                    :data="monthlyCost"
+                    :single-line="false"
+                    :max-height="250"
+                    ref="dataTableInstRef"
+                />
+            </div>
+        </div>
     </div>
 </template>
 
 <style>
 .analysis-page {
     height: 20vh;
+}
+
+.monthly-cost-table-container {
+    display: flex;
+}
+
+.monthly-cost-table-wrapper {
+    margin: auto;
+    width: 20%;
 }
 </style>
