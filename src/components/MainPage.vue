e020c863b2cd2cb5890c888bad79293983b56144
LinHeLurking <LinHe.Lurking@gmail.com>
2022-01-11 19:55:23 +0800

    change upload url

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -60,7 +60,7 @@ export default defineComponent({
     <n-card title="账单分析">
         <div class="upload-container-outer">
             <div class="upload-container-inner">
-                <n-upload :action="getUserDataPath()" :name="getUserFileName()">
+                <n-upload action="upload.php" :name="getUserFileName()">
                     <n-upload-dragger>
                         <div style="margin-bottom: 12px;">
                             <n-icon size="48" :depth="3">
@@ -75,7 +75,7 @@ export default defineComponent({
         <n-divider />
         <div class="upload-container-outer">
             <div class="upload-container-inner">
-                <n-upload :action="getUserDataPath()" :name="getUserFileName()">
+                <n-upload action="upload.php" :name="getUserFileName()">
                     <n-upload-dragger>
                         <div style="margin-bottom: 12px;">
                             <n-icon size="48" :depth="3">
