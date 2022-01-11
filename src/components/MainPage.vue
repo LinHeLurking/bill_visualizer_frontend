44327b2a2c5b3a1a8cd00e75a2c050fc84e4a357
LinHeLurking <LinHe.Lurking@gmail.com>
2022-01-11 19:40:35 +0800

    change uploaded file name

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -39,9 +39,18 @@ export default defineComponent({
                 return str;
             }
         };
+        const getUserFileName = function () {
+            if (currentUser.value == "") {
+                return "user_default";
+            } else {
+                const str = "user_" + currentUser.value;
+                return str;
+            }
+        };
         return {
             gotoAnalysisPage,
             getUserDataPath,
+            getUserFileName,
         };
     },
 });
@@ -51,7 +60,7 @@ export default defineComponent({
     <n-card title="账单分析">
         <div class="upload-container-outer">
             <div class="upload-container-inner">
-                <n-upload :action="getUserDataPath()">
+                <n-upload :action="getUserDataPath()" :name="getUserFileName()">
                     <n-upload-dragger>
                         <div style="margin-bottom: 12px;">
                             <n-icon size="48" :depth="3">
@@ -66,7 +75,7 @@ export default defineComponent({
         <n-divider />
         <div class="upload-container-outer">
             <div class="upload-container-inner">
-                <n-upload :action="getUserDataPath()">
+                <n-upload :action="getUserDataPath()" :name="getUserFileName()">
                     <n-upload-dragger>
                         <div style="margin-bottom: 12px;">
                             <n-icon size="48" :depth="3">
